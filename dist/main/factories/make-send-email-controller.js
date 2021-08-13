213933a633ab3621070ff7d2aa6aa01ed9dc3c36
Hitalo Loose <hitaloose@gmail.com>
2021-08-13 09:13:42 -0300

    config cloud build

--- dist/main/factories/make-send-email-controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeSendEmailController = void 0;
-var send_email_controller_1 = require("../../application/controllers/send-email-controller");
-var make_send_email_usecase_1 = require("./make-send-email-usecase");
-var makeSendEmailController = function () {
-    return new send_email_controller_1.SendEmailController(make_send_email_usecase_1.makeSendEmailUsecase());
-};
-exports.makeSendEmailController = makeSendEmailController;

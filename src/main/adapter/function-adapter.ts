802085331944f956be02a9fd6f10a623bcea8d72
Hitalo Loose <hitaloose@gmail.com>
2021-08-16 13:31:30 -0300

    test

--- src/main/adapter/function-adapter.ts
+++ src/main/adapter/function-adapter.ts
@@ -1,11 +1,11 @@
-import { Context } from "@google-cloud/functions-framework/build/src/functions";
 import { PubsubMessage } from "@google-cloud/pubsub/build/src/publisher";
 
 import { Controller } from "../../application/contracts/controller";
 
 export const functionAdapter = (controller: Controller) => {
-  return (event: PubsubMessage, context: Context) => {
-    console.log(event.data?.toString())
-    console.log(context)
+  return (message: PubsubMessage) => {
+    const data = Buffer.from(message.data as any, 'base64').toString()
+
+    console.log(data)
   }
 }

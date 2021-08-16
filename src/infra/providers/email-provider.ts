ffe148c2416f12d5e2204cfa1a6c01fde6f8d3ba
Hitalo Loose <hitaloose@gmail.com>
2021-08-16 13:57:07 -0300

    add nodemailer

--- src/infra/providers/email-provider.ts
+++ src/infra/providers/email-provider.ts
@@ -1,7 +1,31 @@
+import nodemailer from 'nodemailer'
+
 import { EmailSender } from "../../data/contracts/email-sender";
 import { Mail } from "../../domain/entities/mail";
 
 export class EmailProvider implements EmailSender {
+
   async send(mail: Mail): Promise<void> {
+    const transport = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "6d23d3cfb070fd",
+        pass: "d9d0806eb1bc7e"
+      }
+    });
+
+    await transport.sendMail({
+      to: {
+        name: mail.to.name,
+        address: mail.to.email
+      },
+      from: {
+        name: mail.from.name,
+        address: mail.from.email
+      },
+      subject: mail.subject,
+      text: mail.content
+    })
   }
 }
--- src/main/adapter/function-adapter.ts
+++ src/main/adapter/function-adapter.ts
@@ -1,11 +1,20 @@
+import { Context } from "@google-cloud/functions-framework";
 import { PubsubMessage } from "@google-cloud/pubsub/build/src/publisher";
 
 import { Controller } from "../../application/contracts/controller";
 
 export const functionAdapter = (controller: Controller) => {
-  return (message: PubsubMessage) => {
+  return async (message: PubsubMessage, _: Context, callback: Function) => {
     const data = Buffer.from(message.data as any, 'base64').toString()
 
-    console.log(data)
+    const { statusCode, body } = await controller.run({ body: data })
+
+    if (statusCode >= 200 && statusCode <= 299) {
+      callback(null, 'Success!')
+    }
+
+    if (statusCode >= 400) {
+      callback(body)
+    }
   }
 }
